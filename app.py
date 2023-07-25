3591e192c1d55c9ab1723d433a5a5856f9d57a0a
Anderson <eusouanderson@outlook.com>
2023-07-25 07:41:49 -0300

    "updated"

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ usuarios = read_users()
 # Endpoint para obter a lista completa de usuários
 @app.route('/api/usuarios', methods=['GET'])
 def get_usuarios():
+    usuarios = read_users()
     return jsonify(usuarios)
 
 # Endpoint para obter informações de um usuário específico pelo ID
 @app.route('/api/usuarios/<int:usuario_id>', methods=['GET'])
 def get_usuario(usuario_id):
+
+    usuarios = read_users()
     usuario = next((user for user in usuarios if user['id'] == usuario_id), None)
     if usuario:
         return jsonify(usuario)
@@ -24,7 +27,11 @@ def get_usuario(usuario_id):
 # Endpoint para deletar usuário pelo ID, ou 0 para apagar todos
 @app.route('/api/usuarios/delete/<int:usuario_id>', methods=['GET']) 
 def delete(usuario_id):
+    
+    
     delete_users(usuario_id)
+    
+    usuarios = read_users()
     if usuarios:
         return jsonify(usuarios)
     else:
@@ -33,19 +40,11 @@ def delete(usuario_id):
 # Endpoint para adicionar usuários 
 @app.route('/api/usuarios/adicionar/<string:usuario_name>', methods=['POST', 'GET'])
 def add_users(usuario_name):
-    save_user(name=usuario_name, email='eusouanderson', password='123')
-    updateBD()
-    return jsonify(usuarios)
-
-def updateBD():
     
-    try:
-        users = read_users()
-    except Exception as e:
-        print(f"Erro a atualizar BD {e}")
-    finally:
-        del users
-
+    save_user(name=usuario_name, email='eusouanderson', password='123')
+    
+    usuarios = read_users()
+    return jsonify(usuarios)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
